4f8b176ab54ef2a0504a3b2d9265cc60b05768bc
benhk2005 <benhk2005@users.noreply.github.com>
2015-02-20 20:46:18 +0800

    Performance fine tune and add support on big grid mode

--- hktv.js
+++ hktv.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name       HKTV Mall Stock Marking
 // @namespace  https://github.com/benhk2005/HKTVMallScript
-// @version    0.1
+// @version    0.2
 // @description  enter something useful
 // @match      http://www.hktvmall.com/hktv/zh/search*
 // @match		http://www.hktvmall.com/hktv/zh/main/search*
+// @match      http://www.hktvmall.com/hktv/zh/main/*
 // @copyright  2012+, You
 // ==/UserScript==
 var noStock = $("<div>").text("缺貨").css(
@@ -19,10 +20,17 @@ var noStock = $("<div>").text("缺貨").css(
     }
 );
 
-$('.product-grid-small').each(function(){ 
-    if($(this).find('button.outOfStock').size() > 0){
-        //$(this).closest('.product-grid-small').css('background', 'black');
-        $(this).closest('.product-grid-small').append(noStock.clone());
-        $(this).closest('.product-grid-small').css('opacity', '0.3');
-    }
+stockCheck();
+function stockCheck(){
+    $('.product-grid-small:not(".stockchecked"), .product-grid-big:not(".stockchecked")').each(function(){ 
+        $(this).addClass('stockchecked');
+        if($(this).find('button.outOfStock').size() > 0){
+            $(this).append(noStock.clone());
+            $(this).css('opacity', '0.3');
+        }
+    });
+}
+
+$(document).ajaxComplete(function(){
+   	stockCheck(); 
 });
